1af645e160924ad00c2b8f352f5ad40ccdc9085a
AustinJHealy <56621614+AustinJHealy@users.noreply.github.com>
2020-10-11 23:14:49 -0400

    completed actions router

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-const express = require('express')
-
-const server = express()
-server.use(express.json())
+const express = require('express');
+const userActions = require('./actions/actionsRouter');
+const server = express();
+server.use(express.json());
 
 server.get('/', (req, res) => {
     res.send(`<p>Welcome to the unit 4 build sprint API</p>`)
 });
+
+server.use('/api/actions', userActions);
 
 module.exports = server;
